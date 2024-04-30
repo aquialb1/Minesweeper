c826e1c7ca6addd135b8adca9353fbce9be2bf07
Aquialb <aquialb@gmail.com>
2024-04-30 18:23:48 -0400

    Initial game completion. Still need to fix bug in line 67

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 class Board():
     def __init__(self, dim_size, num_mines):
@@ -76,11 +77,77 @@ class Board():
         return True
     
     def __str__(self):
-        # generate array to visualize board
-        pass
+        # generate array to represent board
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row,col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+                    
+        string_rep = ''
+        # get max column widths for printing
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
 
-def play(dim_size = 10, num_mines = 10):
+        # print csv strings
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
+
+def play(dim_size = 10, num_mines = 20):
     # generate board
     board = Board(dim_size, num_mines)
     
-
+    while len(board.dug) < board.dim_size ** 2 - num_mines:
+        print(board)
+        safe = True
+        
+        # (x,y) or (x, y) or (x,   y)
+        user_input = re.split(',(\\s)*', input("Where do you want to dig? Input as row,col: "))  # '0, 3'
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Please try again.")
+            continue
+        
+        # if loc is valid, dig
+        safe = board.dig(row, col)
+        if not safe:
+            # if we dig a mine
+            break
+        
+        if safe:
+            print("Congrats!")
+        else:
+            print("Game over!")
+            board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+            print(board)
+            
+play()
