81d95d0fb17af196e41b825c857ec5e754bc8e0f
Aquialb <aquialb@gmail.com>
2024-07-05 17:58:15 -0400

    Removed dummy file

--- Hello.py
+++ Hello.py
@@ -3,8 +3,10 @@ from datetime import datetime
 def Hello():
     time = datetime.now()
     current_time = time.strftime('%H:%M:%S')
+    
+    name = input("What is your name?: ")
 
-    print("Hello Albert, the time is:",current_time)
+    print("Hello",name,"the time is:",current_time)
 
 if __name__ == '__main__':
     Hello()
